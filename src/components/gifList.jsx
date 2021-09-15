cd317036b55913683a5068e4a845e73152aea777
Simon Elicaste <simonelicaste@gmail.com>
2021-09-15 15:06:34 +0200

    tested some life components

--- src/components/gifList.jsx
+++ src/components/gifList.jsx
@@ -1,12 +1,21 @@
 import React from 'react';
 import Gif from './gif';
 
-const GifList = (props) => {
-  return (
-    <div className="gif-list">
-      {props.gifs.map(gif => <Gif id={gif} key={gif} selectGif={props.selectGif} />)}
-    </div>
-  );
-};
+// const GifList = (props) => {
+//   return (
+//     <div className="gif-list">
+//       {props.gifs.map(gif => <Gif id={gif} key={gif} selectGif={props.selectGif} />)}
+//     </div>
+//   );
+// };
+
+//destructuring
+const GifList = ({gifs, selectGif}) => {//props.gifs..with curly
+	return (
+	  <div className="gif-list">
+		{gifs.map(gif => <Gif id={gif} key={gif} selectGif={selectGif} />)}
+	  </div>
+	);
+  };
 
 export default GifList;
--- src/components/searchBar.jsx
+++ src/components/searchBar.jsx
@@ -1,14 +1,40 @@
 import React, { Component } from "react";
 
 class SearchBar extends Component {
+	constructor(props) {
+		super(props)
+		this.state = { term: '' }; //we can take and reuse value of this input elsewhere
+	}
+	
 	handleUpdate = (event) => {
+		//change the state of term
+		this.setState({
+			term: event.target.value
+		});
 		this.props.searchFunction(event.target.value);
 		}
-  render () {
-    return (
-	  	<input type="text" className="form-control form-search"  onChange={this.handleUpdate} />
-    );
-  }
+	//   componentWillMount(){
+	// 	  console.log('will mount');
+	//   }
+	//   componentDidMount(){
+	// 	  console.log('did mount');
+	//   }
+	// shouldComponentUpdate(){
+	// 	return false;
+	// }
+
+	render () {
+		console.log('rendered');
+		return (
+			<input
+			value={this.state.term}
+			type="text"
+			className="form-control form-search"
+			onChange={this.handleUpdate}
+
+			/>
+		);
+	}
 }
 
 export default SearchBar;
